314003cd8fc0f531d2e4707325105caa58919de1
arinach <arina.tchukurova@yandex.ru>
2022-09-19 20:58:06 +0300

    add second

---
+++ second/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значения M и N. Напишите программу, которая 
+// найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int m = 5;
+int n = 9;
+void Result (int m, int n)
+{
+    int result = 0;
+    if(m<n)
+    {
+        for(int i = m; i<=n; i++)
+        {
+            result = result+i;
+            
+        }
+        Console.WriteLine(result);
+    }
+        return;
+}
+Result(m,n);
